70b2323b439f869bbb0c84dd76832b8ae98cba35
sienlonglim <limsienlong@gmail.com>
2023-12-02 17:19:51 +0800

    added example

--- pages/02_How_it_works.py
+++ pages/02_How_it_works.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.markdown('### :rocket: How it works')
 st.text('Page in progress')
+
 # st.image('RAG_framework.png')
 # st.text('Hi there, this site is my simple demonstration of the RAG framework. Let me break down the idea part by part')
 # st.header('Part 1 ')
--- pages/03_Examples.py
+++ pages/03_Examples.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
 st.markdown('### :iphone: Examples')
-st.text('Page in progress')
+st.text("Here is an example of a query on the book : Subscrived - Why the Subscription Model Will Be Your Company's Future and What to Do about it. By Tien Tzuo")
 
-st.header('Example')
 st.image('images/example1-1.png')
 st.image('images/example1-2.png')
